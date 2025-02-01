d585446bc104c0ffc9ade618d058565130a6b44a
HalysonSA <HalysonSA@users.noreply.github.com>
2025-02-01 13:44:12 -0300

    feat: return permissions in jwt token and function to check refresh token and return access token

--- src/modules/auth/auth.service.ts
+++ src/modules/auth/auth.service.ts
@@ -3,14 +3,19 @@ import { UserRepositoryPort } from 'src/modules/user/repository/user.repository.
 import { USER_REPOSITORY } from 'src/modules/user/user.token';
 import { LoginRequestDTO } from './dtos/login.dto';
 import * as bcrypt from 'bcrypt';
-import { sign, decode } from 'jsonwebtoken';
-import { InvalidCredentialsError } from './auth.errors';
+import { sign, decode, verify, TokenExpiredError } from 'jsonwebtoken';
+import { InvalidCredentialsError, TokenError } from './auth.errors';
+import { PERMISSIONS_REPOSITORY } from '../permissions/permissions.token';
+import { PermissionsRepositoryPort } from '../permissions/repository/permissions.repository.port';
+import { REFRESH_TOKEN_REPOSITORY } from './auth.token';
+import { RefreshTokenRepositoryPort } from './repository/refreshToken.repository.port';
 
 interface TokenData {
   id: string;
   name: string;
   email: string;
   phone: string;
+  permissions: string[];
 }
 
 @Injectable()
@@ -18,9 +23,13 @@ export class AuthService {
   constructor(
     @Inject(USER_REPOSITORY)
     private readonly userRepo: UserRepositoryPort,
+    @Inject(PERMISSIONS_REPOSITORY)
+    private readonly permissionsRepo: PermissionsRepositoryPort,
+    @Inject(REFRESH_TOKEN_REPOSITORY)
+    private readonly refreshTokenRepo: RefreshTokenRepositoryPort,
   ) {}
 
-  async generateAccessToken(token: string) {
+  private async generateAccessToken(token: string) {
     const decoded = decode(token) as TokenData;
 
     const accessToken = sign(
@@ -29,6 +38,7 @@ export class AuthService {
         name: decoded.name,
         email: decoded.email,
         phone: decoded.phone,
+        permissions: decoded.permissions,
       },
       process.env.SECRET,
       {
@@ -39,10 +49,46 @@ export class AuthService {
     return { accessToken };
   }
 
+  async refreshToken(token: string) {
+    const decoded = decode(token) as TokenData;
+
+    if (!decoded) {
+      throw new Error('Invalid token format');
+    }
+
+    try {
+      const tokenExists = await this.refreshTokenRepo.findToken(
+        token,
+        decoded.id,
+      );
+
+      if (!tokenExists) {
+        throw new TokenError();
+      }
+
+      verify(token, process.env.SECRET);
+    } catch (error) {
+      if (error instanceof TokenExpiredError) {
+        await this.refreshTokenRepo.delete(token);
+        throw new TokenError('Expired token');
+      }
+      if (error instanceof TokenError) {
+        throw new TokenError('Invalid token');
+      }
+
+      throw error;
+    }
+
+    return await this.generateAccessToken(token);
+  }
+
   async execute(props: LoginRequestDTO) {
     const { email, password } = props;
 
     const user = await this.userRepo.findByEmail(email);
+    const userPermissions = await this.permissionsRepo.findPermissionsByUserId(
+      user.id,
+    );
 
     const passwordMatch = await bcrypt.compare(password, user.password);
 
@@ -56,12 +102,16 @@ export class AuthService {
         name: user.name,
         email: user.email,
         phone: user.phone,
+        permissions:
+          userPermissions.map((perm) => `${perm.role}:${perm.action}`) || [],
       },
       process.env.SECRET,
       {
         expiresIn: '7d',
       },
     );
+
+    await this.refreshTokenRepo.create(persistentToken, user.id);
 
     const generatedToken = await this.generateAccessToken(persistentToken);
 
