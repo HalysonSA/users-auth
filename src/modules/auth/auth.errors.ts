343c8a88704c6ee05ff33be1172c2a6334ede3d1
HalysonSA <HalysonSA@users.noreply.github.com>
2025-02-01 13:40:58 -0300

    feat: custom token error

--- src/modules/auth/auth.errors.ts
+++ src/modules/auth/auth.errors.ts
@@ -1,9 +1,19 @@
 export class InvalidCredentialsError extends Error {
   public readonly statusCode: number;
 
-  constructor(message = 'E-mail ou senha inválidos.') {
+  constructor(message = 'invalid Email or password') {
     super(message);
     this.name = 'InvalidCredentialsError';
+    this.statusCode = 401;
+  }
+}
+
+export class TokenError extends Error {
+  public readonly statusCode: number;
+
+  constructor(message = 'Token Error') {
+    super(message);
+    this.name = 'TokenError';
     this.statusCode = 401;
   }
 }
