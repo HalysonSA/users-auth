45e3bcfb69ca4fb1b10429b8d47e7d21a0fb8735
HalysonSA <HalysonSA@users.noreply.github.com>
2025-02-01 13:41:31 -0300

    feat: add refresh token repository

---
+++ src/modules/auth/repository/refreshToken.repository.port.ts
@@ -0,0 +1,7 @@
+import { RefreshToken } from '@prisma/client';
+
+export interface RefreshTokenRepositoryPort {
+  create(token: string, userId: string): Promise<void>;
+  findToken(token: string, userId: string): Promise<RefreshToken>;
+  delete(token: string): Promise<void>;
+}
---
+++ src/modules/auth/repository/refreshToken.repository.ts
@@ -0,0 +1,35 @@
+import { Injectable } from '@nestjs/common';
+import { RefreshTokenRepositoryPort } from './refreshToken.repository.port';
+import { RefreshToken } from '@prisma/client';
+import { PrismaService } from 'src/libs/database/prisma.service';
+
+@Injectable()
+export class RefreshTokenRepository implements RefreshTokenRepositoryPort {
+  constructor(private readonly prisma: PrismaService) {}
+
+  async create(token: string, userId: string): Promise<void> {
+    await this.prisma.refreshToken.create({
+      data: {
+        token,
+        user_id: userId,
+      },
+    });
+  }
+
+  async findToken(token: string, userId: string): Promise<RefreshToken> {
+    return await this.prisma.refreshToken.findFirst({
+      where: {
+        token: token,
+        user_id: userId,
+      },
+    });
+  }
+
+  async delete(token: string): Promise<void> {
+    await this.prisma.refreshToken.delete({
+      where: {
+        token,
+      },
+    });
+  }
+}
